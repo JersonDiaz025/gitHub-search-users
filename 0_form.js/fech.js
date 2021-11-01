cda1cf49c96ab7c324ba7e237472f39d361e3bb5
jersoncuevas <thejer19@gmail.com>
2021-11-01 04:23:57 +0100

    working in the api

--- 0_form.js/fech.js
+++
@@ -1,1 +0,0 @@
-import{input1,send1,inf1} from "./main.js";
--- 0_form.js/main.js
+++ 0_form.js/main.js
@@ -1,7 +1,76 @@
-const a = document;
-const input1 = a.getElementById("uname");
-const send1 = a.getElementById("submit");
-const inf1 = a.getElementById("textInfo");
+const doc = document;
+const input1 = doc.getElementById("submit");
+const dates = doc.getElementById("inf1");
+const urlAPI = `https://api.github.com/users/`
 
-export{input1,send1,inf1};
 
+
+doc.addEventListener("submit",(s)=>{
+    s.preventDefault()
+    if ((s.target[0].value.length) <= 1){
+        alert("Invalid input... A username is expected")
+    }
+    else{   
+        let asigText = `https://api.github.com/users/${s.target[0].value}`
+        s.target[0].value = " "
+        
+        fetch(asigText)
+            .then(response =>response.json())
+            .then(data=>{
+                
+                dates.innerHTML = `<img src="${data.avatar_url}" alt="">
+                                    <p>Name: ${data.login}</p>
+                                    <li>Repository name: ${data.name}</li>
+                                    <li>Followers: ${data.followers}</li>
+                                    // <p href=${data.url}>
+                                    
+                                    `
+        
+            })
+            
+        }
+
+})      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+        
+    
+    
