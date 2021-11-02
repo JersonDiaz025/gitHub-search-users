ea148ca73c07c4b219d586851caf8946d9d9053f
jersoncuevas <thejer19@gmail.com>
2021-11-02 17:20:56 -0400

    updating styles

--- 0_form.js/main.js
+++ 0_form.js/main.js
@@ -7,32 +7,32 @@ doc.addEventListener("submit",(s)=>{
     if ((s.target[0].value.length) <= 1){
         alert("Invalid input... A username is expected")
     }
-    else{   
+    else{
         let asigText = `https://api.github.com/users/${s.target[0].value}`
-        s.target[0].value = " "
+        s.target[0].value = ""
 
         fetch(asigText)
             .then(request =>request.json()) 
             .then(data=>{
                 if(data.message === "Not Found"){
                     alert("(Invalid) .. this name is not associated with any repository")
-                    location.reload()
+                    // location.reload()
                 }else{
-                    dates.innerHTML=`<img src="${data.avatar_url}" alt="">
-                                        <section class= "dates_Styles">
-                                            <li class="li1">NAME:----- ${data.login}</li>
-                                            <li class="li2">REPOSITORY  NAME:----- ${data.name}</li>
-                                            <li class="li3">FOLLOWERS:------- ${data.followers}</li>
-                                            <p class="li4">
-                                                <a href=${data.html_url}>${data.html_url}</a>
-                                            </p>
-                                        </section>
-                                    `
-                    }              
-            })           
-        }
-})      
-
+                    dates.innerHTML=
+                                    `<div class="info_user"><img src="${data.avatar_url}" alt="" class="img_user">
+                                    <section class= "dates_Styles">
+                                        <li class="li1">NAME:----- ${data.login}</li>
+                                        <li class="li2">REPOSITORY  NAME:----- ${data.name}</li>
+                                        <li class="li3">FOLLOWERS:-------  ${data.followers}</li>
+                                        <p class="li4">
+                                            <a href=${data.html_url}>${data.html_url}</a>
+                                        </p>
+                                    </section></div>
+                                    `               
+                    }  
+                })                     
+        }    
+}) 
 
     
     
